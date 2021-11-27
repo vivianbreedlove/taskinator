97938ca1231560f81e2af54c4c98cc224d576957
vivianbreedlove <viviangbreedlove@gmail.com>
2021-11-26 19:49:14 -0600

    handle form submission

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,15 @@
-var buttonEl = document.querySelector("#save-task"); 
+var formEl = document.querySelector("#task-form"); 
 var tasksToDoEl = document.querySelector("#tasks-to-do"); 
 
-var createTaskHandler = function() { 
+var createTaskHandler = function(event) { 
+  event.preventDefault(); 
+
+  var taskNameInput = document.querySelector("input[name='task-name']");
+  console.log(taskNameInput);
   var listItemEl = document.createElement("li"); 
   listItemEl.className = "task-item"; 
   listItemEl.textContent = "This is a new task."; 
   tasksToDoEl.appendChild(listItemEl); 
   }; 
 
-buttonEl.addEventListener("click", createTaskHandler);
+  formEl.addEventListener("submit", createTaskHandler);
